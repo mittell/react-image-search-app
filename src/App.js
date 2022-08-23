d36cde5c8023954f710183e05b3ddaa30f569ddd
Chris Mittell <mittell@hotmail.co.uk>
2022-08-24 08:57:40 +0900

    feat: added Image component and logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { createContext } from 'react';
 import Images from './components/Images';
 import Jumbotron from './components/Jumbotron';
 import SearchField from './components/SearchField';
+import useAxios from './hooks/useAxios';
+
+export const ImageContext = createContext();
 
 function App() {
+	const { response, isLoading, error, fetchData } = useAxios(
+		`search/photos?page=1&query=office&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+	);
+
+	const value = {
+		response,
+		isLoading,
+		error,
+		fetchData,
+	};
+
 	return (
-		<>
+		<ImageContext.Provider value={value}>
 			<Jumbotron>
 				<SearchField />
 			</Jumbotron>
 			<Images />
-		</>
+		</ImageContext.Provider>
 	);
 }
 
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,14 +1,17 @@
+import { useContext } from 'react';
+import { ImageContext } from '../App';
+import Image from './Image';
+
 const Images = () => {
+	const { response, isLoading } = useContext(ImageContext);
+
 	return (
 		<>
 			<h1 className='text-center mt-6 underline text-2xl'>Results for Cats</h1>
-			<div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10'>
-				<h1>Image 1</h1>
-				<h1>Image 2</h1>
-				<h1>Image 3</h1>
-				<h1>Image 4</h1>
-				<h1>Image 5</h1>
-				<h1>Image 6</h1>
+			<div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4'>
+				{response.map((data, key) => (
+					<Image key={key} data={data} />
+				))}
 			</div>
 		</>
 	);
